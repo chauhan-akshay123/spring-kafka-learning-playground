644d0c7e78644e30482aacb2ea95f7e81a075e26
Akshay Chauhan <chauhanakshay395@gmail.com>
2026-08-13 05:46:29 +0530

    Initial Project setup

---
+++ notification-service/notification-service/src/main/java/com/akshay/learnKafka/notification_service/consumer/UserKafkaConsumer.java
@@ -0,0 +1,28 @@
+package com.akshay.learnKafka.notification_service.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserKafkaConsumer {
+
+    @KafkaListener(topics = "user-random-topic")
+    public void handleUserRandomTopic1(String message) {
+
+        log.info("handleUserRandomTopic1 -> message received: {}", message);
+    }
+
+    @KafkaListener(topics = "user-random-topic")
+    public void handleUserRandomTopic2(String message) {
+
+        log.info("handleUserRandomTopic2 -> message received: {}", message);
+    }
+
+    @KafkaListener(topics = "user-random-topic")
+    public void handleUserRandomTopic3(String message) {
+
+        log.info("handleUserRandomTopic3 -> message received: {}", message);
+    }
+}
---
+++ notification-service/notification-service/src/test/java/com/akshay/learnKafka/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.learnKafka.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
